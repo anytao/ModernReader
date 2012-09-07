6921cbdca154432bc7e13fa19b1f2875129c8437
Anytao <anytao@live.com>
2012-09-07 11:44:23 +0800

    create wpreader solution

---
+++ WPReader/Reader/OKr.WPBook.Client.Core/Context/OKrBookContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using OKr.WPBook.Client.Core.Data;
+using OKr.WPBook.Client.Core.Config;
+using OKr.Phone.Common.Context;
+
+namespace OKr.WPBook.Client.Core.Context
+{
+    public class OKrBookContext : OKrContextBase
+    {
+        #region Ctor
+
+        private OKrBookContext()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public static OKrBookContext Current
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public OKrBookConfig Config { get; set; }
+
+        public OKrApp App { get; set; }
+
+        #endregion
+
+        #region Variables
+
+        private static OKrBookContext instance = new OKrBookContext();
+
+        #endregion
+    }
+}
---
+++ WPReader/Reader/OKr.WPBook.Client.Core/Data/Ad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace OKr.WPBook.Client.Core.Data
+{
+    public class Ad
+    {
+        public string AppID { get; set; }
+        public string UnitID { get; set; }
+        public string Type { get; set; }
+        public bool IsShow { get; set; }
+    }
+}
