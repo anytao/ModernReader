d36c3d3db9a181540818dc1e8b271d52431e6af3
Anytao <anytao@live.com>
2012-10-08 17:39:43 +0800

    imp okr reader for load config

--- Win8Reader/Reader/OKr.Win8Book.Client.Core/Context/BookContext.cs
+++ Win8Reader/Reader/OKr.Win8Book.Client.Core/Context/BookContext.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using OKr.Common.Context;
 using OKr.Common.Storage;
 using OKr.Win8Book.Client.Core.Builder;
 using OKr.Win8Book.Client.Core.Data;
+using Windows.ApplicationModel;
+using Windows.Data.Xml.Dom;
+using Windows.Storage;
 
 namespace OKr.Win8Book.Client.Core.Context
 {
@@ -20,8 +25,20 @@ namespace OKr.Win8Book.Client.Core.Context
 
         protected async override Task<Book> DoLoad()
         {
-            var book = await TextParser.GetBook("book\\category.txt");
-            book.Name = "三国演义";
+            var book = await TextParser.GetBook("book\\category.txt");            
+
+            Package package = Package.Current;
+            StorageFolder installedLocation = package.InstalledLocation;
+
+            var file = await StorageFile.GetFileFromPathAsync(Path.Combine(Windows.ApplicationModel.Package.Current.InstalledLocation.Path, @"Assets\Data\me.okr"));
+
+            XmlDocument doc = await XmlDocument.LoadFromFileAsync(file);
+            XDocument xdoc = XDocument.Parse(doc.GetXml());
+
+            var ele = xdoc.Element("config").Element("okrbook");
+            book.Name = ele.Element("name").Value;
+            book.Author = ele.Element("author").Value;
+            book.Desc = ele.Element("intro").Value; 
 
             return book;
         }
