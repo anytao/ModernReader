2ea1cb86cf7f98b50e5027bfd895d5011d6d582d
elecpiano <elecpiano@gmail.com>
2012-10-26 21:52:13 +0800

    font change flyout

--- Win8Reader/Reader/OKr.Win8Book.Client/ViewModel/ChapterGroup.cs
+++ Win8Reader/Reader/OKr.Win8Book.Client/ViewModel/ChapterGroup.cs
@@ -5,6 +5,19 @@ namespace OKr.Win8Book.Client.ViewModel
 {
     public class ChapterGroup
     {
+        private string key = "123";
+        public string Key
+        {
+            get
+            {
+                return key;
+            }
+            set
+            {
+                key = value;
+            }
+        }
+
         private List<Chapter> chapters = new List<Chapter>();
         public List<Chapter> Chapters
         {
